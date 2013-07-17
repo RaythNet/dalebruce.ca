68037cef9926b78afec12f30a386c8874bea226e
Tobias22 <22@dalebruce.ca>
2013-07-16 23:06:40 -0300

    Removed Sldes

--- includes/_header.php
+++ includes/_header.php
@@ -14,28 +14,6 @@ Else { $Title = getTitle($db,$Page); }
 </style>
 <link rel="stylesheet" href="./includes/nivo-slider.css" type="text/css" media="screen" />
 <script src="./includes/js/jquery.min.js" type="text/javascript"></script>
-<script src="./includes/js/jquery.nivo.slider.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(window).load(function() {
-	$('#slider').nivoSlider({
-		effect:'random',
-		slices:15,
-		animSpeed:600,
-		pauseTime:5000,
-		startSlide:0, //Set starting Slide (0 index)
-		directionNav:false, 
-		directionNavHide:false, //Only show on hover
-		controlNav:false, //1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-		pauseOnHover:true, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.7, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
-	});
-});
-</script>
 </head>
 <body>
 
